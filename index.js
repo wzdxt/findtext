6c1990ae4a4dca5b449c766a20b36a14cfc11b11
5eb_100007419017_wzdxt <5eb_100007419017_wzdxt@git.cloud.tencent.com>
2018-12-22 01:56:12 +0800

    save

--- index.js
+++ index.js
@@ -2,36 +2,54 @@
 
 var argv = require('optimist').argv;
 var server = require('crud-file-server');
+var http = require('http');
+var httpProxy = require('http-proxy');
 
-if(!argv.q) {
-	console.log('usage:');
-	console.log('  crud-file-server [options]');
-	console.log('');
-	console.log('this starts a file server using the specified command-line options');
-	console.log('');
-	console.log('options:');
-	console.log('');
-	console.log('  -f file system path to expose over http');
-	console.log('  -h log head requests');
-	console.log('  -p port to listen on (example, 80)');
-	console.log('  -q suppress this message');
-	console.log('  -r read only');
-	console.log('  -v virtual path to host the file server on');
-	console.log('');
-	console.log('example:');
-	console.log('');
-	console.log('  crud-file-server -f c:/ -p 8080 -q -v filez');
-	console.log('');
+if (!argv.q) {
+  console.log('usage:');
+  console.log('  crud-file-server [options]');
+  console.log('');
+  console.log('this starts a file server using the specified command-line options');
+  console.log('');
+  console.log('options:');
+  console.log('');
+  console.log('  -f file system path to expose over http');
+  console.log('  -h log head requests');
+  console.log('  -p port to listen on (example, 80)');
+  console.log('  -q suppress this message');
+  console.log('  -r read only');
+  console.log('  -v virtual path to host the file server on');
+  console.log('');
+  console.log('example:');
+  console.log('');
+  console.log('  crud-file-server -f c:/ -p 8080 -q -v filez');
+  console.log('');
 }
 
-var port  = argv.p || 8080;
-var path  = argv.f || process.cwd();
+var port = argv.p || 8080;
+var path = argv.f || process.cwd();
 var vpath = (argv.v || '').trimLeft();
 var readOnly = argv.r;
 var logHeadRequests = argv.h;
 
-require('http').createServer(function (req, res) {
-	server.handleRequest(vpath, path, req, res, readOnly, logHeadRequests);
+var proxy = httpProxy.createProxyServer({});
+proxy.on('error', console.error);
+
+http.createServer(function (req, res) {
+  if (req.url.startsWith('/proxy/')) {
+    var realUrl = req.url.substr(7).replace('/', '://');
+    req.headers.host = 'github.com';
+    req.url = '';
+    proxy.web(req, res, {target: realUrl});
+    console.info("real url:", realUrl);
+    return;
+  } else {
+    server.handleRequest(vpath, path, req, res, readOnly, logHeadRequests);
+  }
 }).listen(port);
 
 console.log('listening on :' + port + '/' + vpath + ', serving ' + path);
+
+
+
+
--- src/app.js
+++ src/app.js
@@ -3,28 +3,28 @@ const ajaxPromises = require('ajax-promises');
 new Vue({
   el: '#app',
   data: {
-    proxy: 'http',
-    domain: 'product-ci',
-    group: 'company',
-    repository: 'hue-com-batchworkflow',
-    from: 'develop~1',
-    to: 'develop',
+    // protocol: 'http',
+    // domain: 'product-ci',
+    // group: 'company',
+    // repository: 'hue-com-batchworkflow',
+    // from: 'develop~1',
+    // to: 'develop',
+    protocol: 'https',
+    domain: 'github.com',
+    group: 'wzdxt',
+    repository: 'findtext',
+    from: 'master~1',
+    to: 'master',
     gitDiff: '',
   },
   methods: {
     findText: function () {
-      var compareUrl = '/proxy/' + this.proxy + '/' + this.domain + '/' + this.group + '/' + this.repository + '/compare/' + this.from + '...' + this.to;
+      var compareUrl = '/proxy/' + this.protocol + '/' + this.domain + '/' + this.group + '/' + this.repository + '/compare/' + this.from + '...' + this.to;
       console.log(compareUrl);
-      ajaxPromises.get(compareUrl, {
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
-          'Access-Control-Allow-Headers': 'Access-Control-*, Origin, X-Requested-With, Content-Type, Accept',
-        }
-      })
+      ajaxPromises.get(compareUrl)
       .then(function(response) {
-        this.gitDiff = response.data;
-      }, this);
+        this.gitDiff = response;
+      }.bind(this));
     }
   }
-})
+});
